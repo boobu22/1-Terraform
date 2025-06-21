30311ccf30e0223d7b1877846d1d69645eca1fc7
boobu22 <2k22it05@kiot.ac.in>
2025-06-21 11:08:06 +0000

    Updated terraform

--- main.tf
+++ main.tf
@@ -14,6 +14,14 @@ provider "aws" {
 resource "aws_vpc" "my-vpc-1" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    "Name" = "terra-vpc-us"
+    "Name" = "my-us-vpc"
   }
 }
+terraform {
+  backend "s3" {
+    bucket         = "terra-bucket221"
+    key            = "terraform/vpc.tfstate"
+    region         = "us-east-1"
+  }
+}
+
