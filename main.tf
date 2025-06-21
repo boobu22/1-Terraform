9b0b1d556181da2d1c17c67f6b383f513b997639
BOOBALAN A <136874985+boobu22@users.noreply.github.com>
2025-06-21 17:06:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
-# Terraform Block
+# Terraform Block with Backend and Provider
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "terra-bucket221"
+    key    = "terraform/vpc.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
+# VPC Resource
 resource "aws_vpc" "my-vpc-1" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    "Name" = "my-us-vpc"
-  }
-}
-terraform {
-  backend "s3" {
-    bucket         = "terra-bucket221"
-    key            = "terraform/vpc.tfstate"
-    region         = "us-east-1"
-  }
-}
 
+  tags = {
+    Name = "my-us-vpc"
+  }
+}
